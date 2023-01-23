3ab151157702a55ed7ab7bc6bc9abf11c423a344
ucancallmenoy <123302285+ucancallmenoy@users.noreply.github.com>
2023-01-23 12:18:34 +0800

    Add files via upload

---
+++ Prelims/Lab-Demo/app.js
@@ -0,0 +1,3 @@
+var myLogModule = require('./utility/log.js');
+
+myLogModule.warning('Node is running');
---
+++ Prelims/Lab-Demo/node-example.js
@@ -0,0 +1,14 @@
+// Perez, Patrick M.
+//Module 3
+//Week 2 and 3: Basics of Node JS
+
+
+//First program
+console.log("Welcome to WEBSERVER");
+
+//Creating my first function
+function Display(x) {
+    console.log(x);
+}
+
+Display(200);
