3eaf4b191b307e1ff22eeaeb7fb61e59f8ac6b6e
ucancallmenoy <123302285+ucancallmenoy@users.noreply.github.com>
2023-01-30 22:00:46 +0800

    2 files added

---
+++ Prelims/Prelim-Hands-On-Quiz/app.js
@@ -0,0 +1,9 @@
+// Perez, Patrick M.
+// WD - 201
+var calculate = require('./calculations.js');
+
+var rate = 300;
+var hoursWorked = 4;
+var days = 6;
+
+console.log(multiply(rate,hoursWorked,days));
