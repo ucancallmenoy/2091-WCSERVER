85602fa257d0b4ede530002513a205305e2736ca
ucancallmenoy <123302285+ucancallmenoy@users.noreply.github.com>
2023-01-30 22:16:20 +0800

    1 file modified

--- Prelims/Prelim-Hands-On-Quiz/app.js
+++ Prelims/Prelim-Hands-On-Quiz/app.js
@@ -6,4 +6,23 @@ var rate = 300;
 var hoursWorked = 4;
 var days = 6;
 
-console.log(multiply(rate,hoursWorked,days));
+var grossIncome = rate * hoursWorked * days;
+var tax = calculate.multiply(grossIncome,0.1);
+var sss = 1200;
+var pagibig = 300;
+var philhealth = 400;
+var deduction = tax + sss + pagibig + philhealth;
+var netSalary = calculate.subtract(grossIncome,deduction);
+
+console.log();
+console.log('Rate per Hour: ' + rate);
+console.log('Hours Worked: ' + hoursWorked);
+console.log('Days per Week: '+ days);
+console.log();
+console.log('Gross Income: ' + grossIncome);
+console.log('Tax: ' + tax);
+console.log('SSS: ' + sss);
+console.log('Pag-ibig: ' + pagibig);
+console.log('PhilHealth: ' + philhealth);
+console.log('Total Deductions: ' + deduction);
+console.log('The net salary is ' + netSalary);
