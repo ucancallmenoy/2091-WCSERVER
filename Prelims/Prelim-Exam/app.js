3ceb74d8ed7d0b8cabccb739d4a84624fa4711c0
ucancallmenoy <123302285+ucancallmenoy@users.noreply.github.com>
2023-02-08 15:54:48 +0800

    2 files added

---
+++ Prelims/Prelim-Exam/app.js
@@ -0,0 +1,8 @@
+const greet = require('../../Prelims/Lab-Activity-1/myGreetings/greet.js');
+var exam = require('./log.js');
+var name = "Patrick Perez";
+
+console.log();
+exam.greet(name);
+exam.congratulations(name);
+exam.farewell(name);
---
+++ Prelims/Prelim-Exam/log.js
@@ -0,0 +1,13 @@
+var log = {
+    greet:function(name){
+      console.log('Hello, ' + name + '!');
+  },
+    congratulations:function(name){
+      console.log('Congratulations, '+ name + '!');
+  },
+    farewell:function(name){
+      console.log('Goodbye, ' + name + '!');
+  }
+};
+
+module.exports = log
